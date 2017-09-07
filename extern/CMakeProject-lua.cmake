2c3fb68661ae6abd73ed43da55ba83c081c7d384
Nickito12 <abramlujan@gmail.com>
2017-09-06 22:53:04 -0300

    Use Lua JIT 2.0.5

--- extern/CMakeProject-lua.cmake
+++ extern/CMakeProject-lua.cmake
@@ -1,22 +1,22 @@
 
 file(GLOB LUA_HPP
-    "LuaJIT-2.0.4/src/*.h"
+    "LuaJIT-2.0.5/src/*.h"
 )
 file(GLOB LUA_SRC
-    "LuaJIT-2.0.4/src/*.c"
+    "LuaJIT-2.0.5/src/*.c"
 )
 source_group("" FILES ${LUA_SRC})
 source_group("" FILES ${LUA_HPP})
 
-add_library("LuaJIT-2.0.4" STATIC ${LUA_SRC} ${LUA_HPP})
+add_library("LuaJIT-2.0.5" STATIC ${LUA_SRC} ${LUA_HPP})
 
-set_property(TARGET "LuaJIT-2.0.4" PROPERTY FOLDER "External Libraries")
+set_property(TARGET "LuaJIT-2.0.5" PROPERTY FOLDER "External Libraries")
 
 # include_directories(src)
 
 if(MSVC)
-  sm_add_compile_definition("LuaJIT-2.0.4")
+  sm_add_compile_definition("LuaJIT-2.0.5")
 endif(MSVC)
 
-disable_project_warnings("LuaJIT-2.0.4")
+disable_project_warnings("LuaJIT-2.0.5")
 
